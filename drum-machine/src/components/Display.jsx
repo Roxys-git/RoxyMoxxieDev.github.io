ae1a1af7531c3ce4420826cda111dbc302517cf1
Roxana Zwicky <roxanaanastasiazwicky@gmail.com>
2025-02-02 08:36:33 +0100

    fix: redesign porfolio

--- drum-machine/src/components/Display.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-
-const Display = () => {
-  const display = useSelector((state) => state.drum.display);
-  return <div id="display">{display}</div>;
-};
-
-export default Display;
